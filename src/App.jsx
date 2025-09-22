aaad508856b0f21372432039f250aa66e805309a
ub-victor <victoireushindi371@gmail.com>
2025-09-22 14:05:53 +0200

    React Props and desfault and Prototype

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <Student name= "Victoire" age = {30} isStudent={true} />
       <Student name= "Ushindi" age = {20} isStudent={false} />
       <Student name= "Ushindi" age = {18} isStudent={true} />
+      <Student/>
     </>
   );
 }
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,19 +1,25 @@
-import Prototypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-const Student = ({name,age, isStudent}) => {
+const Student = ({ name = "Guest", age = 0, isStudent = false }) => {
     return ( 
         <div className="student">
             <p>Name: {name}</p>
             <p>age: {age} </p>
-            <p>Student: {isStudent?"Yes" : "No"} </p>
+            <p>Student: {isStudent ? "Yes" : "No"} </p>
         </div>
      );
 }
 
-Student.prototypes = {
-        name:Prototypes.string,
-        age: Prototypes.number,
-        isStudent: Prototypes.bool
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool
+};
+// Student.defaultProps = {
+//     name: "Guest",
+//     age: 0,
+//     isStudent: false
+// };Default it react 18+ does not work to make it work you have to paste directly in the Coponant declaration as we did
 
-}
+
 export default Student;
